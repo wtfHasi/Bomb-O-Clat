6d1e56ba44811a87d7aa5a9428f73cc9e9c4e0d1
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-01-31 19:34:06 +0530

    jump move and flipping sprites added

--- bomboclat/scripts/bomb_guy.gd
+++ bomboclat/scripts/bomb_guy.gd
@@ -1,25 +1,34 @@
 extends CharacterBody2D
 
+@onready var sprite := $AnimatedSprite2D  # Reference AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		# Play jump animation if in air
+		sprite.play("jump")
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle jump (Spacebar & W)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sprite.play("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get input direction for movement (Arrow Keys & A/D)
+	var direction := Input.get_axis("move_left", "move_right")
+
 	if direction:
 		velocity.x = direction * SPEED
+		if is_on_floor():  # Only play run animation if on the ground
+			sprite.play("run")
+		# Flip character based on direction
+		sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			sprite.play("idle")  # Play idle animation when not moving
 
 	move_and_slide()
